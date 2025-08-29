8bdc21c76bc94f49c7e6332d08ffc144ce54472f
Carlos Acuña Arias <carlos.acuna.arias@est.una.ac.cr>
2025-08-29 16:44:58 -0600

    second commit

---
+++ Match-3/Gem.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Gem {
+private:
+    int tipo;
+    int fila;
+    int columna;
+    sf::Sprite sprite;
+
+public:
+    Gem() {
+        tipo = 0;
+        fila = 0;
+        columna = 0;
+    }
+
+    Gem(int t, int f, int c, sf::Texture* texturas[]) {
+        tipo = t;
+        fila = f;
+        columna = c;
+        sprite.setTexture(*texturas[tipo]);
+        sprite.setPosition((float)(columna * 64), (float)(fila * 64));
+    }
+
+    void setTipo(int t) {
+        tipo = t;
+    }
+
+    int getTipo() {
+        return tipo;
+    }
+
+    void setFila(int f) {
+        fila = f;
+        sprite.setPosition((float)(columna * 64), (float)(fila * 64));
+    }
+
+    int getFila() {
+        return fila;
+    }
+};
