d2e7c2a14e6d27d4bccc74a85f296af3dbb6b5f6
Carlos Acuña Arias <carlos.acuna.arias@est.una.ac.cr>
2025-08-30 14:02:58 -0600

    third commit

--- Match-3/Board.h
+++ Match-3/Board.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Gem.h"
+#include <cstdlib>
+#include <ctime>
 #include <SFML/Graphics.hpp>
 
 class Board {
@@ -9,6 +11,20 @@ private:
     bool paraEliminar[8][8];
 
 public:
-    Board();
+    Board(sf::Texture* texturasEntrada[]) {
+        texturas = texturasEntrada;
+        llenarTablero();
+    }
+
+    void llenarTablero() {
+        std::srand((unsigned int)std::time(0));
+        for (int fila = 0; fila < 8; fila++) {
+            for (int columna = 0; columna < 8; columna++) {
+                int tipo = std::rand() % 5;
+                matriz[fila][columna] = new Gem(tipo, fila, columna, texturas);
+                paraEliminar[fila][columna] = false;
+            }
+        }
+    }
 };
 
